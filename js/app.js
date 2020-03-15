1907cefee1c723bd5412dd55d448e746cedbdfa7
Javier Gómez <javiergomezve@gmail.com>
2020-03-15 12:24:30 -0500

    [add] sidebar

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-const menuIzquierdo = document.querySelector('.menu-left');
+const menuLeft = document.querySelector('.menu-left');
 
-menuIzquierdo.addEventListener('click', (e)  => {
-       const claseMenu = e.target.classList;
-       
-       // Selecciona el contenedor
-       const contenedor = document.querySelector('.page'),
-             flechaIzq = document.querySelector('.fa-arrow-left'),
-             flechaDer = document.querySelector('.fa-arrow-right');
+menuLeft.addEventListener('click', (e)  => {
+    const claseMenu = e.target.classList;
 
-       if(claseMenu.contains('fa-arrow-left') ) {
-            // cerrar el menú lateral
-            contenedor.classList.add('no-menu');
-            e.target.style.display = 'none';
-            flechaDer.style.display = 'block';
-       } else if( claseMenu.contains('fa-arrow-right')) {
-            contenedor.classList.remove('no-menu');
-            e.target.style.display = 'none';
-            flechaIzq.style.display = 'block';
-       }
+    // Selecciona el contenedor
+    const contenedor = document.querySelector('.page'),
+         flechaIzq = document.querySelector('.fa-arrow-left'),
+         flechaDer = document.querySelector('.fa-arrow-right');
+
+    if(claseMenu.contains('fa-arrow-left') ) {
+        // cerrar el menú lateral
+        contenedor.classList.add('no-menu');
+        e.target.style.display = 'none';
+        flechaDer.style.display = 'block';
+    } else if( claseMenu.contains('fa-arrow-right')) {
+        contenedor.classList.remove('no-menu');
+        e.target.style.display = 'none';
+        flechaIzq.style.display = 'block';
+    }
 });
 
